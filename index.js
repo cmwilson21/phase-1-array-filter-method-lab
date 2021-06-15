acd77cebf34f6bc63ea8974599e0e71749cc0f65
Courtney <cmwilson132@gmail.com>
2021-06-15 08:59:52 -0500

    commit tree 1face8081ccee034871c09b77b2f4b01bf750adb

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution here
+function findMatching(taxiDrivers, driver) {        
+    return taxiDrivers.filter(match =>
+        match.toLowerCase() === driver.toLowerCase()
+    )
+}
+
+function fuzzyMatch(taxiDrivers, drivers, letter) {
+    let filteredNames = taxiDrivers.filter(function (word) {
+        return word.charAt(drivers) === letter;
+    } )
+}
+
+//function filter(names, index, letter) {
+  //  var filteredNames = names.filter(function(word) {
+ //      return word.charAt(index) === letter;
+ //   });
+ //   return filteredNames;
+//}
+
